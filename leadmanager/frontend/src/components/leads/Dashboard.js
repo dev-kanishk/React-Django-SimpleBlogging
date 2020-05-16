ee91eb1b0938f94cc01ee294a5c9300076874fe8
kanishk <kanishksinghal2@gmail.com>
2020-05-17 01:29:03 +0530

    first commit

---
+++ leadmanager/frontend/src/components/leads/Dashboard.js
@@ -0,0 +1,15 @@
+import React,{ Fragment} from 'react'
+import Form from './Forms';
+import Leads from './Leads';
+
+export default function Dashboard() {
+    return (
+        <Fragment>
+            <Form/>
+            <Leads/>
+
+
+        </Fragment>
+       
+    )
+}
